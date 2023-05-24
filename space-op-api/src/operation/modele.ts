c861ae4ca8156358800366f248c8f54ad6a3840c
cheliel <etienne.pernon@ccicampus.fr>
2023-05-24 16:00:38 +0200

    ADD => static operations

--- space-op-api/src/operation/modele.ts
+++ space-op-api/src/operation/modele.ts
@@ -93,6 +93,13 @@ const randomResultButton = (
     ids
 });
 
+const orderedResultButton = (
+    ids: Number []
+):resultButton => ({
+    order: "order",
+    ids
+});
+
 interface resultSwitch {
     ids: Number []
 }
@@ -121,31 +128,59 @@ export const operations = [
     OperationResult(randomResultButton([0, 0, 3, 3]), resultSwitch([0]))
     ), 
 
-() => operationBuilder("Appuyer sur tout les boutons rouge", 
-[   buttonElem(0, "string", "RPZ"),
-    buttonElem(1, "string", "RPZ"),
-    buttonElem(2, "string", "RPZ"),
-    buttonElem(3, "string", "RPZ"),
+() => operationBuilder("Appuyer deux fois sur tout les boutons dont le resultat est pair", 
+[   buttonElem(0, "string", "13 x 2"),
+    buttonElem(1, "string", "2^9"),
+    buttonElem(2, "string", "3^127"),
+    buttonElem(3, "string", "13 x 1"),
     ], 
-    OperationResult(randomResultButton([0,1,2]), resultSwitch([0,1]))
+    OperationResult(randomResultButton([0, 0, 1, 1]), resultSwitch([]))
     ), 
 
-() => operationBuilder("Appuyer sur tout les boutons rouge", 
-[   buttonElem(0, "string", "RPZ"),
-    buttonElem(1, "string", "RPZ"),
-    buttonElem(2, "string", "RPZ"),
-    buttonElem(3, "string", "RPZ"),
+() => operationBuilder("Appuyer 1 fois sur les bouttons de couleur primaire et deux fois sur les autres", 
+[   buttonElem(0, "color", "#ffe436"),
+    buttonElem(1, "color", "#008020"),
+    buttonElem(2, "color", "#5b3c11"),
+    buttonElem(3, "color", "#067790"),
+    buttonElem(3, "color", "#f00020"),
+    buttonElem(3, "color", "#aa00bb"),
     ], 
     OperationResult(randomResultButton([0,1,2]), resultSwitch([0,1]))
     ), 
     
-() => operationBuilder("Appuyer sur tout les boutons rouge", 
-[   buttonElem(0, "string", "RPZ"),
-    buttonElem(1, "string", "RPZ"),
-    buttonElem(2, "string", "RPZ"),
-    buttonElem(3, "string", "RPZ"),
+() => operationBuilder("Activer les switch correspondant à des boite de jeux vidéo inconnus !", 
+[
+    switchElem(0, "string", "Meca Form"),
+    switchElem(1, "string", "Epic Games"),
+    switchElem(2, "string", "Progestuel"),
+    switchElem(3, "string", "Color Dream")
+    
     ], 
-    OperationResult(randomResultButton([0,1,2]), resultSwitch([0,1]))
+    OperationResult(randomResultButton([]), resultSwitch([1, 3]))
+    ), 
+
+() => operationBuilder("Appuyer sur ces chiffres romain dans l'ordre !", 
+[
+    buttonElem(0, "string", "XVII"),
+    buttonElem(1, "string", "XIV"),
+    buttonElem(2, "string", "XV"),
+    buttonElem(3, "string", "XIX"),
+    
+    ], 
+    OperationResult(randomResultButton([1, 2, 0, 3]), resultSwitch([]))
+    ), 
+
+() => operationBuilder("Activer les switchs dont les chivvre romain son pair", 
+[
+    switchElem(0, "string", "IV"),
+    switchElem(1, "string", "XIX"),
+    switchElem(2, "string", "XV"),
+    switchElem(3, "string", "XXII"),
+    switchElem(4, "string", "XXX"),
+    switchElem(5, "string", "XIII")
+    
+    ], 
+    OperationResult(randomResultButton([]), resultSwitch([0, 3, 4]))
     ), 
 
 ]
