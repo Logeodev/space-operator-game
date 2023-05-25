bafc62a512d9aba0758d1b65332cf3b618e57aef
cheliel <etienne.pernon@ccicampus.fr>
2023-05-25 11:34:51 +0200

    instance

--- space-op-api/src/Games/GamesManager.ts
+++ space-op-api/src/Games/GamesManager.ts
@@ -11,11 +11,13 @@ class GamesManager {
     static getInstance() {
       if (!GamesManager.instance)
       GamesManager.instance = new GamesManager();
-      
+
       return GamesManager.instance;
     }
 
 
 }
+
+export default GamesManager.getInstance()
 
 
