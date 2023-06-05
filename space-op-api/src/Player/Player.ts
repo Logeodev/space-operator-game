c96b46577d4e5ac3dec79ab6b7ea83597fe8fe64
Cheliel <etienne.pernon@gmail.com>
2023-06-05 18:46:27 +0200

    ADD => WS connect ADD  => WS broadcast

--- space-op-api/src/Player/Player.ts
+++ space-op-api/src/Player/Player.ts
@@ -1,20 +1,41 @@
-import { uuid } from "uuidv4"
+
 
 export class Player {
    private id:string
    private pseudo:string
+   private socket? : WebSocket
+   private status : boolean
 
-   public constructor(pseudo:string) {
-    this.id = uuid()
+   public constructor(pseudo:string, id : string) {
+    this.id = id
     this.pseudo = pseudo
+    this.status = false
    }
 
-   getId () {
+   public getId () {
     return this.id
    }
 
-   getPseudo () {
-    this.getPseudo
+   public getPseudo () {
+    return this.pseudo
+   }
+
+   public setSocket(socket : WebSocket){
+      if(!this.socket){
+         this.socket = socket
+      }
+   }
+
+   public getSocket() {
+      return this.socket
+   }
+
+   public getStatus() : boolean{
+      return this.status
+   }
+
+   public setStatus(status : boolean){
+      this.status = status
    }
 
 
