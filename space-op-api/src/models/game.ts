8d5d1daa6e7389fdf27c809cffcecbd51a859113
Cheliel <etienne.pernon@gmail.com>
2023-06-05 10:47:09 +0200

    SUPP => node-module

--- space-op-api/src/models/game.ts
+++
@@ -1,6 +0,0 @@
-import { Player } from "./player";
-
-export interface Game {
-    gameId:string,
-    players:Player[]
-}
--- space-op-apk/src/api/connection.tsx
+++
@@ -1,3 +0,0 @@
-const connString = "http://192.168.56.1:5000/api";
-
-export default connString;
